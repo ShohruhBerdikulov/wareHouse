ce624439f8f511ae1380b414052bc9b338f00b0b
Shohruh_Berdikulov <berdikulovshohruh2002@gmail.com>
2021-05-24 15:07:19 +0500

    one

--- src/main/java/ecma/aif/warehouse/repository/OutputProductRepository.java
+++ src/main/java/ecma/aif/warehouse/repository/OutputProductRepository.java
@@ -3,11 +3,12 @@ package ecma.aif.warehouse.repository;
 import ecma.aif.warehouse.entitiy.OutputProduct;
 import ecma.aif.warehouse.entitiy.Product;
 import ecma.aif.warehouse.projection.OutputCustom;
+import ecma.aif.warehouse.projection.OutputProductCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RepositoryRestResource(path = "outputPro", collectionResourceRel = "list",excerptProjection = OutputCustom.class)
+@RepositoryRestResource(path = "outputPro", collectionResourceRel = "list",excerptProjection = OutputProductCustom.class)
 public interface OutputProductRepository extends JpaRepository<OutputProduct,Integer> {
 }
